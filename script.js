b727b958c523913edad483fb39e5788d89cee51f
Ryan P.C. McQuen <ryan.q@linux.com>
2015-06-22 21:44:37 -0700

    hat tip to RamiSarieddine
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-window.onload = function loadInfo() {
-    var data = getJson("https://cdn.rawgit.com/roadsidemultimedia/jsonDump/d6691ff6747be9732eb56bf84858eb08f8e538d3/infoCard1.json");
+
+var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
 };
-document.getElementById("data__name").textContent = data.name;
-document.getElementById("data__id").textContent = data.id;
-document.getElementById("data__htype").textContent = data.htype;
-document.getElementById("data__cms").textContent = data.cms;
-document.getElementById("data__ip").textContent = data.ip;
-document.getElementById("data__resolving").textContent = data.resolving;
-document.getElementById("data__size").textContent = data.size;
-document.getElementById("data__name_server").textContent = data.name_server;
-document.getElementById("data__whois_admin_contact").textContent = data.whois_admin_contact;
+
+window.onload = function loadInfo() {
+    var data = getJSON("https://cdn.rawgit.com/roadsidemultimedia/jsonDump/d6691ff6747be9732eb56bf84858eb08f8e538d3/infoCard1.json");
+    document.getElementById("data__name").textContent = data.name;
+    document.getElementById("data__id").textContent = data.id;
+    document.getElementById("data__htype").textContent = data.htype;
+    document.getElementById("data__cms").textContent = data.cms;
+    document.getElementById("data__ip").textContent = data.ip;
+    document.getElementById("data__resolving").textContent = data.resolving;
+    document.getElementById("data__size").textContent = data.size;
+    document.getElementById("data__name_server").textContent = data.name_server;
+    document.getElementById("data__whois_admin_contact").textContent = data.whois_admin_contact;
 
 
-function getJson(url) {
-  var httpReq = new XMLHttpRequest();
-  httpReq.open("GET", url);
-  httpReq.send(null);
-  return httpReq.responseText;
-}
+};
 
