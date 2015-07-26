b57ae50a134d3263467cd203827d9e0802a09648
Ryan P.C. McQuen <ryan.q@linux.com>
2015-07-26 13:23:36 -0700

    more function refinement
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var getJSON = function(url) {
     xhr.responseType = 'json';
     xhr.onload = function() {
       var status = xhr.status;
-      if (status == 200) {
-        resolve(xhr.responseText);
+      if (status === 200) {
+        resolve(xhr.response);
       } else {
         reject(status);
       }
@@ -17,17 +17,18 @@ var getJSON = function(url) {
 };
 
 window.onload = function loadInfo() {
-    var info = getJSON("https://cdn.rawgit.com/roadsidemultimedia/jsonDump/a9a92a7ea14c444e615bf9fc9c2b99da52a1cc08/infoCard4.json");
-    //var data = JSON.parse(JSON.stringify(info));
-    var data = JSON.parse(info);
-    document.getElementById("data__name").textContent = data.name;
-    document.getElementById("data__id").textContent = data.id;
-    document.getElementById("data__htype").textContent = data.htype;
-    document.getElementById("data__cms").textContent = data.cms;
-    document.getElementById("data__ip").textContent = data.ip;
-    document.getElementById("data__resolving").textContent = data.resolving;
-    document.getElementById("data__size").textContent = data.size;
-    document.getElementById("data__name_server").textContent = data.name_server;
-    document.getElementById("data__whois_admin_contact").textContent = data.whois_admin_contact;
+    getJSON("https://cdn.rawgit.com/roadsidemultimedia/jsonDump/a9a92a7ea14c444e615bf9fc9c2b99da52a1cc08/infoCard4.json").then(function(data) {
+      document.getElementById("data__name").textContent = data.name;
+      document.getElementById("data__id").textContent = data.id;
+      document.getElementById("data__htype").textContent = data.htype;
+      document.getElementById("data__cms").textContent = data.cms;
+      document.getElementById("data__ip").textContent = data.ip;
+      document.getElementById("data__resolving").textContent = data.resolving;
+      document.getElementById("data__size").textContent = data.size;
+      document.getElementById("data__name_server").textContent = data.name_server;
+      document.getElementById("data__whois_admin_contact").textContent = data.whois_admin_contact;
+  }, function(status) {
+    alert('Something is amiss.');
+  });
 };
 
