f1214e20564e1446e0360e096bc9f03bea3348fe
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-07-28 13:44:41 -0700

    cruft clean out

--- script.js
+++ script.js
@@ -19,18 +19,6 @@ var getJSON = function(url) {
 
 window.onload = function loadInfo() {
     getJSON("https://rsmm-domains.firebaseio.com/lagiacrus/domains/bradsammonsdds-com.json").then(function(data) {
-    /*
-    getJSON("https://cdn.rawgit.com/roadsidemultimedia/jsonDump/a9a92a7ea14c444e615bf9fc9c2b99da52a1cc08/infoCard4.json").then(function(data) {
-      document.getElementById("data__name").textContent = data.name;
-      document.getElementById("data__id").textContent = data.id;
-      document.getElementById("data__htype").textContent = data.htype;
-      document.getElementById("data__cms").textContent = data.cms;
-      document.getElementById("data__ip").textContent = data.ip;
-      document.getElementById("data__resolving").textContent = data.resolving;
-      document.getElementById("data__size").textContent = data.size;
-      document.getElementById("data__name_server").textContent = data.name_server;
-      document.getElementById("data__whois_admin_contact").textContent = data.whois_admin_contact;
-    */
       document.getElementById("data__name").textContent = data.name;
       document.getElementById("data__cms").textContent = data.cms;
       document.getElementById("data__ip").textContent = data.ip;
