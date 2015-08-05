7cbddf2099438b17548ba6256c8449cdca695b06
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-08-04 21:36:57 -0700

    works!!!!!!!!!!!

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 // thanks to Mathias Bynens:
 // https://mathiasbynens.be/notes/xhr-responsetype-json
-var getJSON = function(url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-    xhr.send();
-  });
+var getJSON = function (url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            var status = xhr.status;
+            if (status === 200) {
+                resolve(xhr.response);
+            } else {
+                reject(status);
+            }
+        };
+        xhr.send();
+    });
 };
 
-window.onload = function loadInfo() {
-    getJSON("https://rsmm-domains.firebaseio.com/lagiacrus/domains/bradsammonsdds-com.json").then(function(data) {
-      document.getElementById("data__name").textContent = data.name;
-      document.getElementById("data__cms").textContent = data.cms;
-      document.getElementById("data__ip").textContent = data.ip;
-      //document.getElementById("data__last_checked").textContent = data.last_checked;
-      document.getElementById("data__last_checked_hr").textContent = data.last_checked_hr;
-      document.getElementById("data__resolving").textContent = data.resolving;
-      document.getElementById("data__size").textContent = data.size;
-  }, function(status) {
-    alert('Something is amiss.');
-  });
-};
+function loadInfo(json, rootElement) {
+    var oldElem = document.getElementById(rootElement);
+    getJSON(json).then(function (data) {
+        for (var i in data) {
+            var newStuff = document.createDocumentFragment();
+            var newCard = document.createElement("div");
+            newCard.className = "card";
+            newStuff.appendChild(newCard);
+            oldElem.appendChild(newStuff);
 
+            var newTitle = document.createElement('h2');
+            newTitle.innerHTML = i + "<br/>";
+            while (newTitle.firstChild) {
+                newCard.appendChild(newTitle.firstChild);
+            }
+            for (var j in data[i]) {
+                var newInfo = document.createElement('span');
+                newInfo.innerHTML = j + ": " + data[i][j] + "<br/>";
+                while (newInfo.firstChild) {
+                    newCard.appendChild(newInfo.firstChild);
+                }
+            }
+        }
+    });
+}
+
+function init() {
+    loadInfo("https://rsmm-domains.firebaseio.com/lagiacrus/domains.json", "lagiacrus__placeholder");
+    loadInfo("https://rsmm-domains.firebaseio.com/shelbyville/domains.json", "shelbyville__placeholder");
+    loadInfo("https://rsmm-domains.firebaseio.com/springfield/domains.json", "springfield__placeholder");
+}
+window.onload = init();
+
+//
+//
+//
+//
