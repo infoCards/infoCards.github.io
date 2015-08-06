2ce13157c05c4c1c931ccd7e951e8a145623b027
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-08-06 10:13:34 -0700

    add a break before pertinent data

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function loadInfo(json, rootElement) {
             }
             for (var j in data[i]) {
                 var newInfo = document.createElement('span');
-                newInfo.innerHTML = j + ": " + "<span>" + data[i][j] + "</span>" + "<br/>";
+                newInfo.innerHTML = j + ": " + "<br/><span>" + data[i][j] + "</span>" + "<br/>";
                 while (newInfo.firstChild) {
                     newCard.appendChild(newInfo.firstChild);
                 }
