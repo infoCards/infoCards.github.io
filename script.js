a055a151976716a5f8eec05dfb68f461c729f402
Ryan P.C. McQuen <ryan.q@linux.com>
2016-01-24 20:40:01 -0800

    use Object.keys(); general code cleanup

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-(function() {
+/*global window, Promise*/
+/*jslint browser: true, white: true*/
+(function () {
+  'use strict';
   // thanks to Mathias Bynens:
   // https://mathiasbynens.be/notes/xhr-responsetype-json
-  var getJSON = function(url) {
-    return new Promise(function(resolve, reject) {
+  var getJSON = function (url) {
+    return new Promise(function (resolve, reject) {
       var xhr = new XMLHttpRequest();
       xhr.open('get', url, true);
       xhr.responseType = 'json';
-      xhr.onload = function() {
+      xhr.addEventListener('load', function () {
         var status = xhr.status;
         if (status === 200) {
           resolve(xhr.response);
         } else {
           reject(status);
         }
-      };
+      });
       xhr.send();
     });
   };
 
-  function loadInfo(json, rootElement) {
+  var loadInfo = function (json, rootElement) {
     var oldElem = document.getElementById(rootElement);
     oldElem.innerHTML = "<div class='timer-loader'>Loading ...</div>";
-    getJSON(json).then(function(data) {
+    getJSON(json).then(function (data) {
       oldElem.innerHTML = "";
-      for (var i in data) {
+      Object.keys(data).map(function (i) {
         var newStuff = document.createDocumentFragment();
         var newCard = document.createElement("div");
         newCard.className = "card";
         newStuff.appendChild(newCard);
         oldElem.appendChild(newStuff);
-
         var newTitle = document.createElement('h2');
         newTitle.innerHTML = i + "<br/>";
         while (newTitle.firstChild) {
           newCard.appendChild(newTitle.firstChild);
         }
-        for (var j in data[i]) {
+        Object.keys(data[i]).map(function (j) {
           var newInfo = document.createElement('span');
           newInfo.innerHTML = j + ": " + "<br/><span>" + data[i][j] + "</span>" + "<br/>";
           while (newInfo.firstChild) {
             newCard.appendChild(newInfo.firstChild);
           }
-        }
-      }
+        });
+      });
     });
-  }
+  };
 
-  function init() {
+  var init = function () {
     loadInfo("https://rsmm-domains.firebaseio.com/lagiacrus/domains.json", "lagiacrus__placeholder");
     loadInfo("https://rsmm-domains.firebaseio.com/shelbyville/domains.json", "shelbyville__placeholder");
     loadInfo("https://rsmm-domains.firebaseio.com/springfield/domains.json", "springfield__placeholder");
-  }
-  window.onload = init();
+  };
+  window.addEventListener('load', function () {
+    init();
+  });
 
 }());
-
-//
-//
-//
-//
